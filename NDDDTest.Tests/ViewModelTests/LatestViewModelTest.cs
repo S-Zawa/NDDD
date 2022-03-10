8a7c203d1299c92655cd88c41b7f8bb457881e36
S.Aizawa <showta4696@gmail.com>
2022-03-10 23:20:54 +0900

    LatestViewModel Test 追加

---
+++ NDDDTest.Tests/ViewModelTests/LatestViewModelTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NDDD.WinForm.ViewModels;
+
+namespace NDDDTest.Tests.ViewModelTests
+{
+    [TestClass]
+    public class LatestViewModelTest
+    {
+        [TestMethod]
+        public void シナリオ()
+        {
+            var vm = new LatestViewModel();
+        }
+    }
+}
