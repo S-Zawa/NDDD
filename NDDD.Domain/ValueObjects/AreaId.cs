1edb9ec259e83354c61d3142007fb3217e760e9e
S.Aizawa <showta4696@gmail.com>
2022-03-13 22:58:24 +0900

    抽象クラスValueObjectを使用してイコールの問題の解消

---
+++ NDDD.Domain/ValueObjects/AreaId.cs
@@ -0,0 +1,15 @@
+﻿namespace NDDD.Domain.ValueObjects
+{
+    public sealed class AreaId : ValueObject<AreaId>
+    {
+        public AreaId(int value)
+        {
+            Value = value;
+        }
+        public int Value { get; }
+        protected override bool EqualsCore(AreaId other)
+        {
+            return this.Value == other.Value;
+        }
+    }
+}
