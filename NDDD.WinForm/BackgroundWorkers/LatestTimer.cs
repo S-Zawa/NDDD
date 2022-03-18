46e718b21f2faa13745751aeb064d1a0fc7cb253
S.Aizawa <showta4696@gmail.com>
2022-03-18 23:31:31 +0900

    タイマー処理の実装

---
+++ NDDD.WinForm/BackgroundWorkers/LatestTimer.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading;
+
+namespace NDDD.WinForm.BackgroundWorkers
+{
+    internal static class LatestTimer
+    {
+        private static Timer _timer;
+        private static bool _isWork = false;
+
+        static LatestTimer()
+        {
+            _timer = new Timer(Callback);
+        }
+
+        internal static void Start()
+        {
+            _timer.Change(10000, 10000);
+        }
+
+        internal static void Stop()
+        {
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private static void Callback(object o)
+        {
+            if (_isWork)
+            {
+                return;
+            }
+            try
+            {
+                _isWork = true;
+            }
+            finally
+            {
+                _isWork = false;
+            }
+        }
+    }
+}
